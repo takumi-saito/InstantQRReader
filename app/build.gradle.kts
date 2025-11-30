e2efdd9cdfd7b80ad8f383335e0342e712d695d7
takumi-saito <takumi.saitoh.dev@gmail.com>
2025-12-01 06:10:18 +0900

    chore: targetSdk 36へ更新、AGP 8.9.1/Gradle 8.11.1に対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,12 +17,12 @@ plugins {
 
 android {
     namespace = "com.kireaji.instantqrreader"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.kireaji.instantqrreader"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
